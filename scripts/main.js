600d6f09f3cdb262a22ec62a8798205b4621e5d5
fendorei <40180897+fendorei@users.noreply.github.com>
2019-02-20 10:54:24 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+const slider = document.querySelector('.sliderConatiner')
+const sliderImages = document.querySelectorAll('.sliderContainer img')
+const step = 960
+let pos = 0
+let action
+
+function locationSlide() {
+    pos = (pos + 1) % sliderImages.length
+    setPosition(pos)
+}
+
+playSlider()
+
+slider.addEventListener('mouseover', stopSlider)
+slider.addEventListener('mouseout', playSlider)
+
+function setPosition(pos) {
+    slider.style.left = -pos * step + 'px'
+}
+
+function stopSlider() {
+    clearInterval(action)
+}
+
+function playSlider() {
+    action = setInterval(
+        function() {
+            pos = (pos + 1) % sliderImages.length
+            setPosition(pos)
+        },
+        3000
+    )
+}
+
