4df1d203c8a7b19208a2b7b23302dd705e6003a2
Nathan <grigne.nathan@gmail.com>
2019-02-21 01:02:19 +0100

    Update page d'achat

--- HomePod/scripts/main.js
+++ HomePod/scripts/main.js
@@ -1,5 +1,7 @@
 const slider = document.querySelector('.sliderContainer')
 const sliderImages = document.querySelectorAll('.contentSlider img')
+const previous = document.querySelector('.previous')
+const next = document.querySelector('.next')
 const step = -960
 let pos = 0
 let action
@@ -32,4 +34,25 @@ function playSlider() {
     )
 }
 
+previous.addEventListener(
+    'click',
+    function(){
+        pos--
+        if (pos < 0){
+            pos = sliderImages.length-1
+        }
+        sliderContainer.style.left = pos*step + 'px'
+    }
+)
+
+next.addEventListener(
+    'click',
+    function(){
+        pos++
+        if (pos > sliderImages.length-1){
+            pos = 0
+        }
+        sliderContainer.style.left = pos*step + 'px'
+    }
+)
 
