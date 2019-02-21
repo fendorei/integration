1bbda85f53ba306df89779c72da2f9b8e2b2a5b3
Nathan <grigne.nathan@gmail.com>
2019-02-21 05:41:44 +0100

    Update technics page

--- HomePod/scripts/tech.js
+++ HomePod/scripts/tech.js
@@ -1,19 +1,16 @@
-const li = document.querySelectorAll('.technics_description_li')
-
-for(let i=0; i<li.length; i++){
+const li = document.querySelectorAll('.technics_description_li');
+for(let i = 0; i<li.length;i++) {
     li[i].addEventListener(
         'click',
-        function(){
-            if (this.classList.contains('isOpen')){
-                this.classList.remove('isOpen')
-            }
-            else {
-                let temp = document.querySelector('.isOpen')
-                if (temp != null){
-                    temp.classList.remove('isOpen')
+        function () {
+            if(li[i].classList.contains('isOpen'))
+                li[i].classList.remove('isOpen')
+            else{
+                if (document.querySelectorAll('.isOpen').length > 0){
+                    document.querySelector('.isOpen').classList.remove('isOpen')
                 }
+                li[i].classList.add('isOpen')
             }
-            this.classList.add('isOpen')
         }
     )
 }
